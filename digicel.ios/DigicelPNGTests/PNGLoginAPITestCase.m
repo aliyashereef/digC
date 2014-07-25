7605ed9ea6b8c75562cea99c392d4e304d151e9b
vineeth <vineeth@qburst.com>
2014-07-25 14:46:20 +0530

    Added loginWithInvalidId to LoginAPITestCases

--- digicel.ios/DigicelPNGTests/PNGLoginAPITestCase.m
+++ digicel.ios/DigicelPNGTests/PNGLoginAPITestCase.m
@@ -27,7 +27,9 @@
     [super tearDown];
 }
 
-- (void)testLoginWebservice
+#pragma mark - Valid Id
+
+- (void)testLoginWebserviceWithValidId
 {
     __block BOOL isDone             = NO;
     PNGLoginWebservice *logIn       = [[PNGLoginWebservice alloc] init];
@@ -47,6 +49,30 @@
     XCTAssertTrue([self waitFor:&isDone timeout:30],
                   @"Timed out waiting for response asynch method completion");
 }
+
+#pragma mark - Invalid Id
+
+- (void)testLoginWebserviceWithInvalidId
+{
+    __block BOOL isDone             = NO;
+    PNGLoginWebservice *logIn       = [[PNGLoginWebservice alloc] init];
+    NSString *email                 = @"arunadadasddev.s@marker.co.nz";
+    NSString *password              = @"11111";
+    
+    [logIn userLogin:email password:password requestSucceeded:^(PNGUser *user){
+        
+        XCTAssertNotNil(user, @"user is not nil");
+        XCTAssert([user isKindOfClass:[PNGUser class]], @"user is of PNGUser class");
+        isDone = YES;
+    }requestFailed:^(NSString *errorMsg){
+        
+        XCTFail(@"%@",errorMsg);
+        isDone = YES;
+    }];
+    XCTAssertTrue([self waitFor:&isDone timeout:30],
+                  @"Timed out waiting for response asynch method completion");
+}
+
 
 #pragma mark - Wait Function to handle asynchronous call
 - (BOOL)waitFor:(BOOL *)flag timeout:(NSTimeInterval)timeoutSecs {
