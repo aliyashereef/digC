55bf4a00a3230928e13f6261d6df7d201170b170
vineeth <vineeth@qburst.com>
2014-08-04 11:58:41 +0530

    Solved Done button issue...removed zoom out

--- digicel.ios/DigicelPNG/ViewControllers/PNGFullScreenViewController.m
+++ digicel.ios/DigicelPNG/ViewControllers/PNGFullScreenViewController.m
@@ -2,17 +2,15 @@
 //  PNGFullScreenViewController.m
 //  DigicelPNG
 //
-//  Created by qbadmin on 31/07/14.
+//  Created by Subins Jose on 31/07/14.
 //  Copyright (c) 2014 Marker Studio. All rights reserved.
 //
 
 #import "PNGFullScreenViewController.h"
+#import "UIImageView+WebCache.h"
 
-@interface PNGFullScreenViewController ()
-{
-    CGPoint imageCenter, newCenter;
-    CGRect frame;
-}
+
+@interface PNGFullScreenViewController ()   
 
 @end
 
@@ -22,7 +20,6 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
     }
     return self;
 }
@@ -31,43 +28,23 @@
 {
     [super viewDidLoad];
     [self.fullScreenImageView setImageWithURL:[NSURL URLWithString:self.imageUrl] placeholderImage:[UIImage imageNamed:PNGStoryboardImageArticleList]];
-    imageCenter = _fullScreenImageView.center;
-    frame = _fullScreenImageView.frame;
-   
 }
 
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView{
-    if (newCenter.y < imageCenter.y) {
-        _fullScreenImageView.center = imageCenter;
-    }
     return _fullScreenImageView;
-}
-
-- (void)scrollViewDidZoom:(UIScrollView *)scrollView{
-    newCenter = _fullScreenImageView.center;
 }
 
 - (IBAction)DoneButton:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 
 
